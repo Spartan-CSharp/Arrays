993c61659f38461f51cabb2e4a80f7a3aaa1e71d
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 17:04:34 -0500

    Create Code
    Create an array of 3 names. Ask the user which number to select. When the user gives you a number, return that name. Make sure to check for invalid numbers.

--- ArraysConsoleApp/Program.cs
+++ ArraysConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] names = new string[] { "Tim", "Sue", "Bob" };
+bool isValidNumber;
+int number;
+
+do
+{
+	Console.Write("Enter a number from 0 to 2: ");
+	string? numberText = Console.ReadLine();
+
+	isValidNumber = int.TryParse(numberText, out number);
+
+	if ( number < 0 || number > 2 )
+	{
+		isValidNumber = false;
+	}
+
+	if ( !isValidNumber )
+	{
+		Console.WriteLine("That is not a valid number; please try again.");
+	}
+} while ( !isValidNumber );
+
+Console.WriteLine($"The name at position {number} is {names[number]}.");
